0b51b4557df61abb00735d09383da3df09f32f28
Ozodbek Tolliyev <ozodbektolliyev179@gmail.com>
2025-12-28 14:56:40 +0500

    Update task04.py

--- task04.py
+++ task04.py
@@ -1,1 +1,18 @@
+text = input('Input: ')
 
+
+def get_domains(text: str) -> list[str]:
+    emails = text.split(',')
+    
+    domains = []
+    for email in emails:
+        idx = email.find('@')
+        if idx != -1:
+            domain = email[idx:]
+            domains.append(domain)
+
+    return domains
+
+
+domains = get_domains(text=text)
+print(domains)
