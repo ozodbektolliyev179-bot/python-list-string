7e858f1fe99dbae48c0a8dce864b5bd4d9340fe0
Ozodbek Tolliyev <ozodbektolliyev179@gmail.com>
2025-12-28 14:54:13 +0500

    Update task02.py

--- task02.py
+++ task02.py
@@ -1,1 +1,14 @@
+faoliyatlar = input("Faoliyat turlarini kiriting: ")
+
+items = faoliyatlar.split(",")
+
+snake_case_items = []
+
+for item in items:
+    item = item.strip().lower()     
+    item = item.replace(" ", "_")   
+    snake_case_items.append(item)
+
+result = "_".join(snake_case_items)
+print(result)
 
